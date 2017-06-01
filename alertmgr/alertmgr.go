b9668ee0183239ba426484e6b5d71d89b52e1342
Matt Snoby <snobym@cisco.com>
2017-06-01 15:13:53 -0400

    updated the method prototype because it was just wrong

--- alertmgr/alertmgr.go
+++ alertmgr/alertmgr.go
@@ -11,19 +11,13 @@ type AlertMgr struct {
 	buf []byte
 }
 
-func (alert AlertMgr) AlertMgr_LoadRawData(buf []byte) (err error) {
+func (alert *AlertMgr) AlertMgr_LoadRawData(buf []byte) (err error) {
 	fmt.Println(" Loading buffer into AlertMgr memory")
-	x := len(buf)
-	fmt.Println(" Length of data : ", x)
-
 	alert.buf = buf
-
-	y := len(alert.buf)
-	fmt.Println(" Length of alertMgr.buf : ", y)
 	return nil
 }
 
-func (alert AlertMgr) AlertMgr_PrintRawJSON() (err error) {
+func (alert *AlertMgr) AlertMgr_PrintRawJSON() (err error) {
 	if alert.buf == nil {
 		return errors.New("AlertMgr: No data loaded.  Load data first!!!")
 	}
